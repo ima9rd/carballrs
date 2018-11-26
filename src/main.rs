f011ec38a2a1f83ff7c5bbfce75fc9e0ca5d7443
Mark J <ima9rd@gmail.com>
2018-11-25 21:31:54 -0600

    It actually works!

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+extern crate glob;
+extern crate reqwest;
+extern crate serde_json;
+extern crate version_compare;
+
+mod rattletrap;
+
 fn main() {
-    println!("Hello, world!");
+    rattletrap::check_version::check_version();
 }
--- src/rattletrap/check_version.rs
+++ src/rattletrap/check_version.rs
@@ -1,10 +1,5 @@
-extern crate glob;
-extern crate reqwest;
-extern crate serde_json;
-extern crate version_compare;
-
-use glob::glob;
-use version_compare::{Version, VersionCompare};
+use ::glob::glob;
+use ::version_compare::{Version, VersionCompare};
 
 const URL: &str = "https://api.github.com/repos/tfausak/rattletrap/releases/latest";
 const RATTLETRAP_PATH: &str = "src/rattletrap/";
@@ -34,27 +29,36 @@ fn scan_dir(s: &str, files: &mut Vec<String>) {
 }
 
 fn version_from_fname(fname: String) -> String {
-    let s: String = fname.clone();
-    let ver: Option<Version> = Version::from(s.as_str());
+    let s: String = fname.to_owned();
+    let str_vec: Vec<&str> = s.split("-").collect();
+    let ver_str = str_vec[str_vec.len() - 2];
+    let ver: Option<Version> = Version::from(ver_str);
     ver.unwrap().as_str().to_owned()
 }
 
-fn main() {
+pub fn check_version() {
     let s: String = fetch_url(URL).unwrap();
     let j: serde_json::Value = parse_json(s).unwrap();
     let github_ver: &str = j["name"].as_str().unwrap();
     let mut files: Vec<String> = Vec::new();
     scan_dir(RATTLETRAP_PATH, &mut files);
-    println!("{:?}", files);
     for file in files {
         let f: String = file.to_owned();
         let mut ver_str: String = version_from_fname(f).as_str().to_owned();
         if ver_str.chars().count() > 0 {
-            if VersionCompare::compare_to(&github_ver, ver_str.as_str(), &version_compare::CompOp::from_sign(">").unwrap()).unwrap() {
+            if VersionCompare::compare_to(
+                &github_ver,
+                ver_str.as_str(),
+                &version_compare::CompOp::from_sign(">").unwrap(),
+            ).unwrap()
+            {
                 println!("Update available!");
                 println!("GitHub version: {}", github_ver);
-                println!("Current version: {}", Version::from(ver_str.as_str()).unwrap().as_str());
-            break
+                println!(
+                    "Current version: {}",
+                    Version::from(ver_str.as_str()).unwrap().as_str()
+                );
+                break;
             }
         }
     }
